2f6f86acdcfd81252aa751c885ae7c56731b5294
Yog <yog.panjarale@gmail.com>
2021-12-21 23:51:48 +0530

    line collsion working

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prefer-const */
 import { Vector } from './vector';
 let canvas: HTMLCanvasElement;
@@ -14,26 +15,67 @@ const [width, height] = [800, 800];
 //delta time
 const dt = 10;
 
-const ball = {
-    radius:50,
-    position: new Vector(300,300),
-    velocity: new Vector(0.1,0.3),
-    color: 'rgb(255,100,52)',
+interface Ball {
+	radius: number;
+	color: string;
+	position: Vector;
+	velocity: Vector;
 }
+const ball: Ball = {
+	radius: 50,
+	position: new Vector(100, 300),
+	velocity: new Vector(0.3, 0.3),
+	color: 'rgb(255,100,52)'
+};
 
-let p1 = new Vector(400,450);
-let p2 = new Vector(300,400);
+let p1 = new Vector(100, 450);
+let p2 = new Vector(300, 400);
 function test() {
-    p1.debugDraw(ctx)
-    p2.debugDraw(ctx)
-    let lineV = p2.minus(p1);
-    lineV.debugDraw(ctx);
+	// p1.debugDraw(ctx, null, 1, false, 'red');
+	// p2.debugDraw(ctx, null, 1, false, 'yellow');
+	let lineV = p2.minus(p1);
+	lineV.debugDraw(ctx, p1);
+	let p1toBall = ball.position.minus(p1);
+	p1toBall.debugDraw(ctx, p1, 1, false, 'gold');
+	let proj = p1toBall.clampedProj(lineV);
+	proj.debugDraw(ctx, p1, 1, false, 'aqua');
+	let displacement = p1toBall.minus(proj);
+	displacement.debugDraw(ctx, p1.plus(proj), 1, false, 'tomato');
 }
 
+function lineBounce(line: Vector[], ball: Ball) {
+    let [p1,p2] = line;
+	let lineV = p2.minus(p1);
+	let p1toBall = ball.position.minus(p1);
+	let proj = p1toBall.clampedProj(lineV);
+	let displacement = p1toBall.minus(proj);
+    let distance = displacement.norm();
+    let overlap = ball.radius - distance;
+    if (distance <=ball.radius){
+        //bounce !
+        ball.velocity.reflect(displacement);
+        displacement.setNorm(overlap);
+        ball.position.add(displacement);
+    }
+
+    //draw vectors
+	lineV.debugDraw(ctx, p1);
+	p1toBall.debugDraw(ctx, p1, 1, false, 'gold');
+	proj.debugDraw(ctx, p1, 1, false, 'aqua');
+	displacement.debugDraw(ctx, p1.plus(proj), 1, false, 'tomato');
+}
+function checkLineBounce() {
+	let line = [p1, p2];
+	lineBounce(line, ball);
+}
+function onMouseMove(ev: MouseEvent) {
+	ball.position.x = ev.pageX;
+	ball.position.y = ev.pageY;
+}
 function draw(): void {
 	clearCanvas();
-    test()
 	drawCircle(ball.position.x, ball.position.y, ball.radius, ball.color);
+	test();
 }
 function clearCanvas() {
 	ctx.clearRect(0, 0, width, height);
@@ -58,6 +100,7 @@ function checkEdgeBounce() {
 function frame() {
 	moveCircle();
 	checkEdgeBounce();
+	checkLineBounce();
 	draw();
 }
 
@@ -65,12 +108,13 @@ function animate(): void {
 	setTimeout(animate, dt);
 	frame();
 }
-
+// this function is needed cause this project is server-side rendered and the window is not available
 function init(): void {
 	canvas = <HTMLCanvasElement>document.querySelector('canvas#game');
 	ctx = canvas.getContext('2d');
 	canvas.width = width;
 	canvas.height = height;
+	window.addEventListener('mousemove', onMouseMove);
 }
 
 function start(): void {
